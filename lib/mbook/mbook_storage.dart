9bafde2241e57b06b15ebdeb26dcf79f8cdc7841
Aymen <bensalahaymen433@gmail.com>
2021-09-30 21:54:21 +0100

    refact some code

--- lib/mbook/mbook_storage.dart
+++ lib/mbook/mbook_storage.dart
@@ -1,22 +1,19 @@
 import 'dart:convert';
 import 'dart:io';
-
-import 'package:path_provider/path_provider.dart';
-
+import 'package:mbook/helpers/data_type_heper.dart';
 import '../helpers/map_helper.dart';
 import '../helpers/path_helper.dart';
 
 class MbookStorage {
-
   Future<T> insert<T>(book, key, value) async {
     await File(await PathHelper.temporaryDirectory + "/$book/$key.txt")
         .create(recursive: true);
     File f = File(await PathHelper.temporaryDirectory + "/$book/$key.txt");
-    if (_isSimpleType(value)) {
+    if (DataTypeHelper.isSimpleType(value)) {
       f.writeAsString(value.toString());
       return value;
     }
-    if (_isIterable(value)) {
+    if (DataTypeHelper.isIterable(value)) {
       _saveList(value, f);
       return value;
     }
@@ -27,8 +24,9 @@ class MbookStorage {
   void _saveList(value, File f) {
     var savableList = [];
     for (var elem in value) {
-      savableList
-          .add(!_isSimpleType(elem) ? MapHelper.objectToMap(elem) : elem);
+      savableList.add(!DataTypeHelper.isSimpleType(elem)
+          ? MapHelper.objectToMap(elem)
+          : elem);
     }
     f.writeAsString(jsonEncode(savableList));
   }
@@ -40,15 +38,4 @@ class MbookStorage {
   // }
 
   delete(String key) {}
-
-  bool _isSimpleType(value) {
-    return value.runtimeType.toString().toLowerCase() == "string" ||
-        value.runtimeType.toString().toLowerCase() == "int" ||
-        value.runtimeType.toString().toLowerCase() == "double" ||
-        value.runtimeType.toString().toLowerCase() == "bool";
-  }
-
-  bool _isIterable(value) {
-    return value is Iterable;
-  }
 }
